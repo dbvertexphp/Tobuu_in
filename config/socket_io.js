37f96a3f6f2c9ed7ef71a57210b650bcc3731443
dbvertexphp <128798599+dbvertexphp@users.noreply.github.com>
2024-01-23 19:17:17 +0530

    code live and update

--- config/socket_io.js
+++ config/socket_io.js
@@ -35,36 +35,39 @@ const createSocketIO = (server) => {
 
             socket.on("stop typing", (room) => {
                   socket.in(room).emit("stop typing");
-                   console.log("stop typing");
+                  console.log("stop typing");
             });
 
             socket.on("block Status", (data) => {
                   const { chatId, status, userId } = data;
                   socket.in(chatId).emit("block Status", { status, userId });
-                   console.log("block Status");
+                  console.log("block Status");
             });
 
             socket.on("new message", (newMessageRecieved) => {
-                  // Access the chat property from the response
-                  var chat = newMessageRecieved.response.chat;
-                     console.log("new message" + newMessageRecieved);
-                  // Check if chat and users are defined
-                  if (!chat || !chat.users) {
-                        return console.log("Chat or chat.users not defined");
+                  try {
+                        var chat = newMessageRecieved.response.chat;
+                        console.log("new message" + newMessageRecieved);
+
+                        if (!chat || !chat.users) {
+                              return console.log(
+                                    "Chat or chat.users not defined"
+                              );
+                        }
+                        chat.users.forEach((user) => {
+                              if (
+                                    user._id ==
+                                    newMessageRecieved.response.sender._id
+                              )
+                                    return;
+                              socket.in(user._id).emit(
+                                    "message received",
+                                    newMessageRecieved
+                              );
+                        });
+                  } catch (error) {
+                        console.error("Error in new message event:", error);
                   }
-
-                  // Iterate through users in the chat
-                  chat.users.forEach((user) => {
-                        // Check if the user is the sender
-                        if (user._id == newMessageRecieved.response.sender._id)
-                              return;
-
-                        // Emit the message to the user
-                        socket.in(user._id).emit(
-                              "message received",
-                              newMessageRecieved
-                        );
-                  });
             });
 
             socket.on("disconnect", () => {
