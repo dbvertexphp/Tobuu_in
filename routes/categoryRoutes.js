729668529568a8684e48fd22196ec74f5a6e3c98
dbvertexphp <128798599+dbvertexphp@users.noreply.github.com>
2024-02-07 19:26:42 +0530

    code live and uplaode issue solved

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,6 +4,7 @@ const {
       GetAllCategories,
       DeleteCategory,
       GetSingleCategoryByName,
+      GetAllCategoriesAdmin,
 } = require("../controllers/categoryControllers.js");
 const protect = require("../middleware/authMiddleware.js");
 
@@ -11,6 +12,7 @@ const categoryRoutes = express.Router();
 
 categoryRoutes.route("/createCategory").post(protect, Createcategory);
 categoryRoutes.route("/").get(GetAllCategories);
+categoryRoutes.route("/GetAllCategoriesAdmin").get(GetAllCategoriesAdmin);
 categoryRoutes.route("/GetCategoryByName").post(GetSingleCategoryByName);
 categoryRoutes.route("/DeleteCategory").post(protect, DeleteCategory);
 
